f75071818cf5910dde7ea7fd28fb5bfca1c57b9a
hotteok00 <hojun7889@gmail.com>
2025-06-16 20:36:44 +0900

    feat: launch

---
+++ collaboration_launch/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'collaboration_launch'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='lhj',
+    maintainer_email='hojun7889@gmail.com',
+    description='Integrated launch for collaboration-2',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
