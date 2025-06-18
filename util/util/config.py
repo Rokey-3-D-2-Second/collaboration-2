a09a52d835f6ae70974ddd34a105e24ec0217399
hotteok00 <hojun7889@gmail.com>
2025-06-18 09:58:27 +0900

    add: params for ros2_controller

--- util/util/config.py
+++ util/util/config.py
@@ -2,26 +2,19 @@
 # cb_interfaces
 valid_targets = ["fork", "knife", "spoon"]
 valid_task_steps = ["move_target", "move_tray", "move_home", "force", "close_grip", "open_grip"]
+# valid_task_steps = ["move_target", "move_tray", "move_home", "hold_target", "release_target", "close_grip", "open_grip"]
 
 TARGET='/target'
 TARGET_COORD='/target_coord'
 TASK_STEPS='/task_steps'
 
 # ======================================================== #
-# image_processor
-
-CAMERA_INFO="/camera/camera/color/camera_info"
-COLOR_IMAGE="/camera/camera/color/image_raw/compressed"
-ALIGNED_IMAGE="/camera/camera/aligned_depth_to_color/image_raw"
-
-# ======================================================== #
-# realsense_camera
-
-# ======================================================== #
 # ros2_controller
+from DR_common2 import posx, posj
 
 # controller
-ROBOT_ID = 'dsr01'
+# ROBOT_ID = 'dsr01'
+ROBOT_ID = ''
 ROBOT_MODEL = 'mo6o9'
 ROBOT_TCP = 'GripperDAmoon'
 ROBOT_TOOL = 'Tool moon'
@@ -29,6 +22,15 @@ VEL = 100
 ACC = 100
 
 # mover
+scanx = [
+    # posx([585.9119873046875, 7.041282653808594, 479.5486755371094, 96.65674591064453, 179.85296630859375, -173.46255493164062]),
+    # posx([143.47349548339844, 7.150089263916016, 479.7123718261719, 97.31204223632812, -179.94760131835938, -172.81277465820312]),
+    posx([619.0982055664062, 2.6109795570373535, 210.99949645996094, 89.5136947631836, 179.8441162109375, 179.3989715576172]),
+    posx([150.7991180419922, 1.8854933977127075, 211.10487365722656, 90.82644653320312, -179.95298767089844, -179.30828857421875,]),
+]
+homej = posj([0, -16, 95, 0, 101, 90])
+homex = posx([251.9128875732422, 7.273128509521484, 479.5822448730469, 78.13613891601562, -179.99046325683594, 167.9998321533203])
+trayx = posx([652.49, 16.510, 40.42, 90.0, 180.0, 180.0])
 
 # forcer
 
@@ -36,6 +38,23 @@ ACC = 100
 GRTIPPER_NAME = 'rg2'
 TOOLCHARGER_IP = '192.168.1.1'
 TOOLCHARGER_PORT = '502'
+
+# moviter
+JOINT_NAMES = ['joint_1', 'joint_2', 'joint_3', 'joint_4', 'joint_5', 'joint_6']
+
+MOTION_IKIN="/motion/ikin"
+FOLLOW_JOINT_TRAJECTORY="/dsr_moveit_controller/follow_joint_trajectory"
+
+# ======================================================== #
+# image_processor
+
+CAMERA_INFO="/camera/camera/color/camera_info"
+COLOR_IMAGE="/camera/camera/color/image_raw/compressed"
+ALIGNED_IMAGE="/camera/camera/aligned_depth_to_color/image_raw"
+GET_CURREN_POSX = ROBOT_ID + "/aux_control/get_current_posx"
+
+# ======================================================== #
+# realsense_camera
 
 # ======================================================== #
 # vui
